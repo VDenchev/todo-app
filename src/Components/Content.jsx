16e18128afbfa88c009ffab25798338114c90987
Viktor Denchev <viktor.denchev02@gmail.com>
2022-06-29 14:47:08 +0300

    Add the functionality to create a new todo and push it to an array

--- src/Components/Content.jsx
+++ src/Components/Content.jsx
@@ -1,13 +1,15 @@
-import React from "react"
+import React, { useState } from "react"
 import List from "./List.jsx"
 import Title from "./Title.jsx"
 import TodoInput from "./TodoInput.jsx"
 
 const Content = () => {
+	const [todoList, setTodoList] = useState([])
+	console.log(todoList)
 	return (
 		<div className="absolute top-12 left-1/2 -translate-x-1/2 w-[87%]">
 			<Title />
-			<TodoInput />
+			<TodoInput handleEnterKeyPress={setTodoList} />
 			<List></List>
 		</div>
 	)
--- src/Components/TodoInput.jsx
+++ src/Components/TodoInput.jsx
@@ -1,19 +1,43 @@
-import React from "react"
+import React, { useState } from "react"
 
-const TodoInput = () => {
+const TodoInput = ({ handleEnterKeyPress }) => {
+	const [todo, setTodo] = useState({ isCompleted: false, message: "" })
+
+	const updateTodo = (e) => {
+		const key = e.target.name
+		const value = key === "isCompleted" ? e.target.checked : e.target.value
+		setTodo((prevTodo) => ({ ...prevTodo, [key]: value }))
+	}
+
+	const handleKeyDown = (e) => {
+		if (e.keyCode === 13) {
+			handleEnterKeyPress((prevState) => [todo, ...prevState])
+			resetTodo()
+		}
+	}
+
+	const resetTodo = () => {
+		setTodo({ isCompleted: false, message: "" })
+	}
+	console.log(todo)
 	return (
 		<div className="bg-white rounded-md p-3 flex items-center mb-4 gap-3 isolate shadow-sm">
 			<input
 				type="radio"
-				name=""
+				name="isCompleted"
+				checked={todo.isCompleted}
+				onChange={updateTodo}
 				id=""
 				className="w-5 h-5 before:-z-[1] before:-m-[2px] cursor-pointer appearance-none rounded-full border border-light-blue-300 checked:border-transparent before:content-check before:absolute before:rounded-full before:w-6 before:h-6 before:bg-gradient-to-br before:text-center before:leading-7 overflow-hidden before:from-gradient1 before:to-gradient2 before:scale-0 before:checked:scale-100 before:transition-all before:delay-200 delay-200 hover:border-transparent box-content before:hover:scale-100 bg-white checked:bg-transparent transition-all"
 			/>
 			<input
 				type="text"
-				name=""
+				name="message"
 				id=""
 				placeholder="Create a new todo..."
+				value={todo.message}
+				onChange={updateTodo}
+				onKeyDown={handleKeyDown}
 				className="bg-transparent w-10/12 text-sm text-light-blue-500 pt-1 focus:outline-none caret-blue leading-none"
 			/>
 		</div>
