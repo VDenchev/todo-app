b7614dcf4ad0025107dd70538a9fddd5dc963920
Viktor Denchev <viktor.denchev02@gmail.com>
2022-07-03 11:49:34 +0300

    Add a 'dark' class to body and set darkMode setting to 'class'

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -3,6 +3,9 @@ import Sun from "../Images/icon-sun.svg"
 import Moon from "../Images/icon-moon.svg"
 const Title = () => {
 	const [isDarkMode, setIsDarkMode] = useState(false)
+
+	document.body.className = isDarkMode ? "dark" : ""
+
 	return (
 		<div className="flex justify-between mb-8 md:mb-10">
 			<h1 className="text-white text-xl md:text-3xl tracking-[0.5rem] font-bold">
