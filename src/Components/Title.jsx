08e6072ed01cc9f86695fee3bd34769abac89551
Viktor Denchev <viktor.denchev02@gmail.com>
2022-07-03 11:33:35 +0300

    Add isDarkMode state

--- src/Components/Title.jsx
+++ src/Components/Title.jsx
@@ -1,15 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import Sun from "../Images/icon-sun.svg"
 import Moon from "../Images/icon-moon.svg"
 const Title = () => {
+	const [isDarkMode, setIsDarkMode] = useState(false)
 	return (
 		<div className="flex justify-between mb-8 md:mb-10">
 			<h1 className="text-white text-xl md:text-3xl tracking-[0.5rem] font-bold">
 				TODO
 			</h1>
-			<input type="checkbox" name="DarkMode" id="DarkMode" className="hidden" />
+			<input
+				type="checkbox"
+				name="DarkMode"
+				id="DarkMode"
+				checked={isDarkMode}
+				onChange={() => setIsDarkMode((prevIsDarkMode) => !prevIsDarkMode)}
+				className="hidden"
+			/>
 			<label htmlFor="DarkMode" className="cursor-pointer">
-				<img src={Moon} alt="toggle" className="w-[22px] md:w-[30px]" />
+				<img
+					src={isDarkMode ? Sun : Moon}
+					alt="toggle"
+					className="w-[22px] md:w-[30px]"
+				/>
 			</label>
 		</div>
 	)
