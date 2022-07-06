1a234bfb56e28a1c60fd252f0ea9ddb3aea2e8f4
Viktor Denchev <viktor.denchev02@gmail.com>
2022-07-06 18:50:03 +0300

    Make Option a controlled component

--- src/Components/Option.jsx
+++ src/Components/Option.jsx
@@ -8,11 +8,12 @@ const Option = ({ text, isChecked }) => {
 				name="buttons"
 				id={text}
 				checked={isChecked}
+				onChange={(e) => (isChecked = e.target.checked)}
 				className="hidden [&+*]:checked:text-blue"
 			/>
 			<label
 				htmlFor={text}
-				className="font-bold cursor-pointer hover:text-light-blue-500 hover:dark:text-dark-blue-200 transition duration-200">
+				className="font-bold cursor-pointer hover:text-light-blue-500 hover:dark:text-dark-blue-200 transition duration-75">
 				{text}
 			</label>
 		</>
